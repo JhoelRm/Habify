c83fde1916804e083b1137a1fca73160767c71d1
JhoelRm <jhoelhramirez923@gmail.com>
2026-04-28 23:28:04 -0500

    cambio en rama

---
+++ docs/test.py
@@ -0,0 +1,9 @@
+print("Cambio desde mi rama")
+
+app_name = "Habify v 1.12"
+description = "App móvil para mejorar hábitos diarios mediante seguimiento, personalización y enfoque integral de bienestar."
+
+def info():
+    return f"{app_name}: {description}"
+
+print(info())
