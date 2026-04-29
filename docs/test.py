15d88bc092548f8a164153acd8ae821dc5c6ff36
Diego Paredes Guerrero <diegovalenteparedes@gmail.com>
2026-04-28 23:29:16 -0500

    fix: elimino repo anidado

---
+++ docs/test.py
@@ -0,0 +1,9 @@
+print("Cambio desde mi rama")
+
+app_name = "Habify XD"
+description = "App móvil para mejorar hábitos diarios mediante seguimiento, personalización y enfoque integral de bienestar UTP"
+
+def info():
+    return f"{app_name}: {description}"
+
+print(info())
